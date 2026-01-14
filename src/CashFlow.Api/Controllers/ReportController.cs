36de70643bc208a69899e2a7f787e0ad168cebfe
Yang Andrade <yangandradeb@gmail.com>
2026-01-13 22:09:43 -0300

    Add ReportController with Excel file download endpoint
    Introduced ReportController in CashFlow.Api.Controllers with a new GET api/report/excel endpoint. This endpoint returns an Excel file as a download if data is present, or a 204 No Content response otherwise. Includes response type annotations for API documentation.

---
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReportController : ControllerBase
+{
+    [HttpGet("excel")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetExecel()
+    {
+        byte[] file = new byte[1];
+
+        const string FileDownloadName = "report.xlsx";
+       if(file.Length > 0)
+            return File(file, MediaTypeNames.Application.Octet, FileDownloadName);
+
+       return NoContent();
+    }
+}
